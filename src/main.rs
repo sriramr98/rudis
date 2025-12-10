faa8bd5a8e0002c1488eb6acda888ae37b2ac3d3
Sriram Ramasamy <sriramr083@gmail.com>
2025-12-10 22:05:32 +0530

    supporting multiple commands with same connection

--- src/main.rs
+++ src/main.rs
@@ -27,16 +27,16 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    let mut buf = [0u8; 512];
-    let bytes_read = stream.read(&mut buf)?;
+    loop {
+        let mut buf = [0u8; 512];
+        let bytes_read = stream.read(&mut buf)?;
 
-    let command =  str::from_utf8(&buf[0..bytes_read])?;
+        let command =  str::from_utf8(&buf[0..bytes_read])?;
 
-    println!("Received command {}", command);
-    let write_result = stream.write_all(b"+PONG\r\n");
-    if write_result.is_err() {
-        println!("Unable to write")
+        println!("Received command {}", command);
+        let write_result = stream.write_all(b"+PONG\r\n");
+        if write_result.is_err() {
+            println!("Unable to write")
+        }
     }
-
-    Ok(())
 }
