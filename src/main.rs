b6b157c062609b076667a9bbfbe5e649972768a8
Sriram Ramasamy <sriramr083@gmail.com>
2025-12-10 22:02:28 +0530

    added PING->PONG

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+use anyhow::Result;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment the code below to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                if let Err(e) = handle_connection(stream) {
+                    println!("{}", e)
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let mut buf = [0u8; 512];
+    let bytes_read = stream.read(&mut buf)?;
+
+    let command =  str::from_utf8(&buf[0..bytes_read])?;
+
+    println!("Received command {}", command);
+    let write_result = stream.write_all(b"+PONG\r\n");
+    if write_result.is_err() {
+        println!("Unable to write")
+    }
+
+    Ok(())
 }
